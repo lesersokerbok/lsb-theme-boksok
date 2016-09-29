4f23cdf463e106211e1f65fb297ccb7f272e130f
Benedicte Raae <raae@bgraphic.no>
2016-09-29 17:44:57 +0200

    Taxonomy lsb_cat frontpage in place

--- taxonomy.php
+++ taxonomy.php
@@ -1,16 +1,15 @@
  <div class="block block-lsb-search">
   <div class="container">
-      <div class="row app-align-center">
-        <div class="col-sm-12 col-md-8 col-md-offset-2">
-          <form>
-            <div class="input-group input-group-lg">
-              <input type="text" class="form-control" placeholder="Søk etter tittel, forfatter, tema ...">
-              <span class="input-group-btn">
-                <button class="btn btn-default" type="button">Søk</button>
-              </span>
-            </div>
-          </form>
-        </div>
+    <div class="row app-align-center">
+      <div class="col-sm-12 col-md-8 col-md-offset-2">
+        <form>
+          <div class="input-group input-group-lg">
+            <input type="text" class="form-control" placeholder="Søk etter tittel, forfatter, tema ...">
+            <span class="input-group-btn">
+              <button class="btn btn-default" type="button">Søk</button>
+            </span>
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -20,23 +19,12 @@
   <?php get_template_part('templates/page-sections'); ?>
 <? endif; ?>
 
-<?php if ( have_posts() ): ?>
-  <section class="loop">
-    <?php if( !is_paged() && have_rows('lsb_page_sections', get_queried_object()) ) : ?>
-    <div class="page-section-header">
-      <h1>
-        <?php esc_html_e( 'Arkiv', 'lsb' ); ?> <small> | <?php echo roots_title(); ?></small>
-      </h1>
-    </div>
-    <?php endif; ?>
-    <?php while (have_posts()) : the_post(); ?>
-      <?php get_template_part('templates/content-summary', get_post_type()); ?>
-    <?php endwhile; ?>
-  </section>
-<?php endif; ?>
+<?php 
 
-<?php if ($wp_query->max_num_pages > 1) : ?>
-  <nav class="post-nav">
-    <?php roots_pagination(); ?>
-  </nav>
-<?php endif; ?>
+  if(is_tax( 'lsb_tax_lsb_cat')) {
+    $lsb_cat = get_queried_object();
+    include(locate_template('templates/lsb-cat-block.php'));
+  }
+?>
+
+  
