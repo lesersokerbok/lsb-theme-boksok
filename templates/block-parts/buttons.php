be0ea7d4585198839b0ce1321bf4d7da1a8f18b0
Benedicte Raae <raae@bgraphic.no>
2016-10-04 14:34:21 +0200

    Better taxonomy archive

--- templates/block-parts/buttons.php
+++ templates/block-parts/buttons.php
@@ -1,6 +1,7 @@
 <?php 
 
   $buttons_title = $lsb_block_part_args['title'];
+  $buttons_description = $lsb_block_part_args['description'];
   $buttons_terms = $lsb_block_part_args['button_terms'];
 
 ?>
@@ -8,6 +9,7 @@
 <?php if( $buttons_title ) : ?>
 <h1 class="block-lsb-heading"><?= $buttons_title ?></h1>
 <?php endif ?>
+<?= $buttons_description ?>
 <div class="block-lsb-selection">
   <?php foreach( $buttons_terms as $term ) : ?>
     <a class="btn btn-default" href="<?= get_term_link($term) ?>"><?= $term->name ?></a>
