aec44196a9f2a2fdf4f185b3483c033bf6fdea66
Benedicte Raae <raae@bgraphic.no>
2016-10-06 13:02:58 +0200

    Use different template for paged lsb_tax_lsb_tag

--- lib/filters.php
+++ lib/filters.php
@@ -4,3 +4,18 @@ function capitalize_title( $term_title ) {
   return ucfirst($term_title);
 }
 add_filter ( 'single_term_title', 'capitalize_title', 0 );
+
+function use_taxonomy_template( $template ) {
+  global $wp_query;
+	
+	if ( is_tax( 'lsb_tax_lsb_cat' ) && $wp_query->query['paged'] > 1 ) {
+		$new_template = locate_template( array( 'index.php' ) );
+		if ( '' != $new_template ) {
+			return $new_template ;
+		}
+	}
+
+	return $template;
+}
+
+add_filter( 'template_include', 'use_taxonomy_template', 99 );
