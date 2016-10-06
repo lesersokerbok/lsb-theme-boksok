a18890bbe12fff157ab446aed1987166da79a90e
Benedicte Raae <raae@bgraphic.no>
2016-10-06 20:10:09 +0200

    Better search form styling

--- searchform.php
+++ searchform.php
@@ -13,23 +13,7 @@
 
 <form role="search" method="get" class="search-form">
   <div class="input-group input-group-lg">
-    <?php if ( !empty($filter_options) ) : ?>
-    <span class="input-group-btn">
-      <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <span><?php echo $filter_name ?></span>
-        <span class="caret"></span>
-      </button>
-      <ul class="dropdown-menu">
-        <li>
-          <?php foreach($filter_options as $option) : ?>
-            <a href="<?php echo $option['url'] ?>" data-tax-term='<?php echo json_encode($option) ?>'><?php echo $option['term_label'] ?></a>
-          <?php endforeach; ?>
-        </li>
-        <li role="separator" class="divider"></li>
-        <li><a href="#"><?php echo $filter_name ?></a></li>
-      </ul>
-    </span>
-    <?php endif; ?>
+    <span class="input-group-addon" id="basic-addon1"><span class="icon icon-magnifying-glass"></span></span>
     <input id="algolia-auto-search" class="form-control" name="s" value="<?php echo $input_value ?>" placeholder="<?php echo $input_placeholder ?>">
     <span class="input-group-btn">
       <button class="btn btn-default" type="submit"><?php echo $submit_text ?></button>
