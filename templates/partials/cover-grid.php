49585bd69cac09c59f568d1c65c6de4fc2ce3010
Benedicte Raae <raae@bgraphic.no>
2016-10-06 07:17:30 +0200

    Newest books in cover-grid.

--- templates/partials/cover-grid.php
+++ templates/partials/cover-grid.php
@@ -7,7 +7,7 @@
     'posts_per_page' => 6,
     'post_type' => 'lsb_book',
     'orderby' => 'date',
-    'order' => 'DESC',
+    'order' => 'DESC', // Newest books
     'tax_query' => [
       [
         'taxonomy' => $cover_grid_tax_term->taxonomy,
