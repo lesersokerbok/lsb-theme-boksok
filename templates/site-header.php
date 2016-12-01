be2e4dda593027f2762fa1f4205de6b2ecac1937
Benedicte Raae <raae@bgraphic.no>
2016-12-01 15:54:12 +0100

    Better main category selection in header #47

--- templates/site-header.php
+++ templates/site-header.php
@@ -2,56 +2,56 @@
   <div class="container">
 
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle text-uppercase collapsed" data-target="#stage" data-toggle="stage" data-distance="-250">
+      <button type="button" class="btn btn-deafult navbar-toggle text-uppercase collapsed" data-target="#stage" data-toggle="stage" data-distance="-250">
         <span class="icon icon-menu"></span>
         <?php esc_html_e( 'Meny', 'lsb' ); ?>
       </button>
-
-      <a class="navbar-brand" href="<?php echo home_url(); ?>">
-        <span class="hidden"><?= get_bloginfo() ?></span>
-			  <?php get_template_part('templates/logo'); ?>
-      </a>
-
       <ul class="nav navbar-nav">
-        <li class="text-uppercase <?= get_lsb_cat_filter_term() ? 'hidden-xs' : ''?>">
-          <a class="p-l-0" href="<?php echo home_url(); ?>"><?= get_bloginfo() ?></a>
-        </li>
-        <?php if(get_lsb_cat_filter_term()) : ?>
         <li>
-          <a class="p-l-0" href="<?= get_term_link(get_lsb_cat_filter_term()) ?>">
-            <?= lsb_capitalize_title(get_lsb_cat_filter_term()->name) ?>
+          <a class="lsb-brand" href="<?php echo home_url(); ?>">
+            <?php get_template_part('templates/logo'); ?>
+            <span class="text-uppercase"><?= get_bloginfo() ?></span>
           </a>
         </li>
+      </ul>
+    </div>
+
+    <nav class="navbar-collapse collapse">
+
+      <div class="btn-group navbar-btn">
+        <?php if(get_lsb_cat_filter_term()) : ?>
+          <a class="btn btn-default" href="<?= get_term_link(get_lsb_cat_filter_term()) ?>">
+            <?= lsb_capitalize_title(get_lsb_cat_filter_term()->name) ?>
+          </a>
         <?php else : ?>
-        <li class="dropdown hidden-xs hidden-sm">
-          <a class="p-l-0" href="#" dropdown-toggle data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-            <?php _e('Velg kategori', 'lsb-theme-boksok'); ?>
-          <span class="caret"></span></a>
-          <?php
+          <a href="<?php echo home_url(); ?>" class="btn btn-default">
+            <?php _e('Alle boktyper', 'lsb-theme-boksok'); ?>
+          </a>
+        <?php endif; ?>
+
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <span class="caret"></span>
+          <span class="sr-only">Toggle Dropdown</span>
+        </button>
+        <?php
             if (has_nav_menu('frontpage_sections')) :
               wp_nav_menu(array('theme_location' => 'frontpage_sections', 'menu_class' => 'dropdown-menu'));
             endif;
           ?>
-        </li>
-        <?php endif ?>
-      </ul>
-    </div>
+      </div>
 
-    <nav class="navbar-collapse collapse text-uppercase navbar-right">
+      <div class="navbar-right">
 
-      <?php
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
-        endif;
-      ?>
+        <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+          endif;
+        ?>
 
-      <ul class="nav navbar-nav">
-        <li class="<?= is_search() ? 'active' : '' ?>">
-          <a href="<?= get_lsb_cat_filter_term() ? get_term_link(get_lsb_cat_filter_term()) : home_url() ?>?s=">
-            <span class="icon icon-magnifying-glass"> </span><?php _e('Søk', 'lsb_boksok') ?>
-          </a>
-        </li>
-      </ul>
+        <button type="button" class="btn btn-default navbar-btn">
+          <span class="icon icon-magnifying-glass"> </span><?php _e('Søk', 'lsb_boksok') ?>
+        </button>
+      </div>
 
     </nav><!--/.nav-collapse -->
 
