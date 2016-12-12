ebd5a6de407f0dbdcdb2ba67c19432926c23062c
Benedicte Raae <raae@bgraphic.no>
2016-12-12 15:27:40 +0100

    More padding on container in main #30

--- templates/articles/archive.php
+++ templates/articles/archive.php
@@ -1,10 +1,8 @@
-<div class="container">
-  <header class="lsb-header text-sm-center m-b-lg">
-    <h1 class="lsb-heading"><?= lsb_page_title() ?></h1>
-    <hr>
-  </header>
+<header class="lsb-header text-sm-center m-b-lg">
+  <h1 class="lsb-heading"><?= lsb_page_title() ?></h1>
+  <hr>
+</header>
 
-  <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/articles/summary'); ?>
-  <?php endwhile; ?>
-</div>
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/articles/summary'); ?>
+<?php endwhile; ?>
--- templates/books/archive.php
+++ templates/books/archive.php
@@ -1,12 +1,10 @@
-<div class="container">
-  <header class="lsb-header text-sm-center m-b-lg">
-    <h1 class="lsb-heading"><?= lsb_page_title() ?></h1>
-    <hr>
-  </header>
+<header class="lsb-header text-sm-center m-b-lg">
+  <h1 class="lsb-heading"><?= lsb_page_title() ?></h1>
+  <hr>
+</header>
 
-  <div class="lsb-book-collection">
-    <?php while (have_posts()) : the_post(); ?>
-      <?php get_template_part('templates/books/summary'); ?>
-    <?php endwhile; ?>
-  </div>
+<div class="lsb-book-collection">
+  <?php while (have_posts()) : the_post(); ?>
+    <?php get_template_part('templates/books/summary'); ?>
+  <?php endwhile; ?>
 </div>
