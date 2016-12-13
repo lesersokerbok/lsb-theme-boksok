5ec92806f936dce4e8981a93e61ac0342dc6629e
Benedicte Raae <raae@bgraphic.no>
2016-12-13 13:12:05 +0100

    Term description on book archives #39

--- templates/books/archive.php
+++ templates/books/archive.php
@@ -1,9 +1,20 @@
+<?php if(!term_description()): ?>
 <header class="lsb-header text-sm-center m-b-lg">
   <h1 class="lsb-heading"><?= lsb_page_title() ?></h1>
   <hr>
 </header>
+<?php endif; ?>
 
 <div class="lsb-book-collection">
+  <?php if(term_description()): ?>
+  <aside class="lsb-book-collection-intro">
+    <header class="lsb-header">
+      <h1 class="lsb-heading"><?= lsb_page_title() ?></h1>
+      <hr>
+    </header>
+    <?= term_description() ?>
+  </aside>
+  <?php endif; ?>
   <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('templates/books/summary'); ?>
   <?php endwhile; ?>
