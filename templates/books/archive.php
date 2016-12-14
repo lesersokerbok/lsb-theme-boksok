2462f5e588b0a9b28e9603bc2991264856f5e27e
Benedicte Raae <raae@bgraphic.no>
2016-12-14 12:16:01 +0100

    Added pagination to tax description #39

--- templates/books/archive.php
+++ templates/books/archive.php
@@ -1,18 +1,20 @@
-<?php if(!term_description() || is_paged()): ?>
-<header class="lsb-header text-sm-center m-b-lg">
+<?php if(!term_description()): ?>
+<header class="lsb-header text-sm-center">
   <h1 class="lsb-heading"><?= lsb_page_title() ?></h1>
   <hr>
 </header>
 <?php endif; ?>
 
 <div class="lsb-book-collection">
-  <?php if(term_description() && !is_paged()): ?>
+  <?php if(term_description()): ?>
   <aside class="lsb-book-collection-intro">
     <header class="lsb-header">
       <h1 class="lsb-heading"><?= lsb_page_title() ?></h1>
       <hr>
     </header>
     <?= term_description() ?>
+    <?php previous_posts_link( __("Tilbake", 'lsb_boksok' ) ); ?>
+    <?php next_posts_link( __("Neste side med bøker", 'lsb_boksok' ) ); ?>
   </aside>
   <?php endif; ?>
   <?php while (have_posts()) : the_post(); ?>
