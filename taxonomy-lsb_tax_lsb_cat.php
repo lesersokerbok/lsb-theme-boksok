0738963208a3461eb5ffd42fa0e3dcc143f00956
Benedicte Raae <raae@bgraphic.no>
2016-12-14 12:18:34 +0100

    Added tax description to tax frontpages as well #39

--- taxonomy-lsb_tax_lsb_cat.php
+++ taxonomy-lsb_tax_lsb_cat.php
@@ -1,3 +1,19 @@
+<div class="lsb-book-collection">
+
+  <aside class="lsb-book-collection-intro">
+    <header class="lsb-header">
+      <h1 class="lsb-heading"><?= lsb_page_title() ?></h1>
+      <hr>
+    </header>
+    <?= term_description() ?>
+    <?= get_next_posts_link(sprintf(__('Gå til alle bøker i <strong>%s</strong>', 'lsb_boksok'),lsb_page_title() )); ?>
+  </aside>
+
+  <?php while (have_posts()) : the_post(); ?>
+    <?php get_template_part('templates/books/summary'); ?>
+  <?php endwhile; ?>
+</div>
+
 <?php if( have_rows('lsb_page_sections', get_queried_object()) ) : ?>
   <?php get_template_part('templates/page-sections/loop' ); ?>
 <?php endif; ?>
